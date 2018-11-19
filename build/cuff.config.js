553c21ae1305650e759dc6f939c35938c7942260
Aelto <hottou.thibault@gmail.com>
2018-11-19 23:48:32 +0100

    switching to yaml instead of toml

--- build/cuff.config.js
+++ build/cuff.config.js
@@ -3,14 +3,17 @@ const { resolve } = require('path')
 const SRC_DIR = resolve(__dirname, '../src')
 const VCPKG_X86_STATIC = `/files/cpp/vcpkg/installed/x86-windows-static`
 const VCPKG_X86 = `/files/cpp/vcpkg/installed/x86-windows`
-const VCPKG_INCLUDE = resolve(VCPKG_X86, 'include')
-const VCPKG_LIB = resolve(VCPKG_X86, 'lib')
+const VCPKG_INCLUDE = resolve(VCPKG_X86_STATIC, 'include')
+const VCPKG_LIB = resolve(VCPKG_X86_STATIC, 'lib')
 
 const config = {
   entry: resolve(SRC_DIR, 'main.cpp'),
   includeDirectories: [
     SRC_DIR,
     VCPKG_INCLUDE
+  ],
+  staticLibraries: [
+    resolve(VCPKG_LIB, 'libyaml-cppmd.lib')
   ],
   args: ['/EHsc'],
   macros: ['DEBUG']
--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,15 @@
 #include <iostream>
 #include <string>
-#include <toml/toml.h>
-
-toml::Value * get_config(char * config_path);
-std::string get_string(toml::Value * config, char * key);
+#include <yaml-cpp/yaml.h>
 
 int main() {
-  auto ifs = std::ifstream("config.toml");
-  auto config = toml::parse(ifs);
+  auto config = YAML::LoadFile("config.yaml");
 
-  if (!config.valid()) {
-    std::cout << "error when parsing config, " << config.errorReason << "\n";
-
-    return 1;
+  if (config["project"] && config["project"]["name"]) {
+    std::cout 
+      << config["project"]["name"].as<std::string>() 
+      << "\n";
   }
-
-  auto & config_content = config.value;
-
-  auto project_name = get_string(&config_content, "project.name");
-  std::cout << project_name << std::endl;
 
   return 0;
-}
-
-std::string get_string(toml::Value * config, char * key) {
-  #ifdef DEBUG
-    std::cout << "looking for key: " << key << "\n";
-  #endif
-
-  const auto * value = config->find(key);
-
-  if (!value || !value->is<std::string>()) {
-    #ifdef DEBUG
-      std::cout << "value not found for key: " << key << " | " << value << "\n";
-    #endif
-
-    return std::string();
-  }
-
-  return value->as<std::string>();
-  }
-
-toml::Value * get_config(char * config_path) {
-  auto ifs = std::ifstream(config_path);
-  auto config = toml::parse(ifs);
-
-  if (!config.valid()) {
-    std::cout << "error when parsing config, " << config.errorReason << "\n";
-
-    return nullptr;
-  }
-
-  return &config.value;
 }
