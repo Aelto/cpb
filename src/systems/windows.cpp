03ac7a35cd3a8dcb940b270165c2b93b79f29662
Thibault H <Aelto@users.noreply.github.com>
2018-11-27 17:15:35 +0100

    use mtime to check if a file rebuild is necessary
    currently ignore changes on the header files, which is quite important...

--- src/systems/windows.cpp
+++ src/systems/windows.cpp
@@ -1,12 +1,37 @@
 #include "windows.h"
 
 #include <iostream>
+#include <experimental/filesystem>
+#include <fstream>
+#include <chrono>
+
+namespace fs = std::experimental::filesystem;
 
 bool systems::execute_windows(
   Project * project,
   SystemOptions * system_options,
   SystemOptions * default_system_options,
   CpbOptions & cpb_options) {
+
+  bool full_rebuild = false;
+  auto dotfile_path = fs::current_path() / fs::path(".cpb"); 
+  auto ymlfile_path = fs::current_path() / fs::path("cpb.yaml");
+  
+  if (fs::exists(dotfile_path)) {
+    auto dotfile_wtime = fs::last_write_time(dotfile_path);
+    auto ymlfile_wtime = fs::last_write_time(ymlfile_path);
+
+    if (ymlfile_wtime > dotfile_wtime) {
+      full_rebuild = true;
+    }
+    
+    fs::last_write_time(dotfile_path, std::chrono::system_clock::now());
+  }
+  else {
+    full_rebuild = true;
+
+    std::ofstream(dotfile_path.c_str());
+  }
 
   if (cpb_options.allow_commands) {
     auto before_commands = system_options
@@ -63,6 +88,37 @@ bool systems::execute_windows(
           ->directory.value_or(".")
     );
   for (auto file_name : files) {
+    auto file = fs::path(directory) / file_name;
+    if (!fs::exists(file)) {
+      std::cout
+        << "could not find file: "
+        << file_name
+        << "\n";
+
+      continue;
+    }
+
+    auto file_wtime = fs::last_write_time(file);
+    auto obj_file = fs::current_path() / fs::path(file_name).filename().replace_extension(".obj");
+
+    if (fs::exists(obj_file) && !full_rebuild) {
+      auto obj_wtime = fs::last_write_time(obj_file);
+
+      #ifdef DEBUG
+        std::cout
+          << "found obj file: "
+          << obj_file.filename()
+          << "\n";
+      #endif
+
+      if (obj_wtime > file_wtime) {
+        // file_wtime older than obj_wtime
+        command += obj_file.filename().string() + " ";
+
+        continue;
+      }
+    }
+
     command += directory + file_name + " ";
   }
 
